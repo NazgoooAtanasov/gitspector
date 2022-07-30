1f04b65d81e783f20f7930f44d849e74f7be7b23
NazgoooAtanasov <naskodragomirov@gmail.com>
2022-07-31 01:33:34 +0300

    feat: segregating branch listing

---
+++ lua/gitspector/branches.lua
@@ -0,0 +1,38 @@
+local pickers = require "telescope.pickers"
+local finders = require "telescope.finders"
+local conf = require("telescope.config").values
+local actions = require("telescope.actions")
+local action_state = require("telescope.actions.state")
+
+local M = {}
+
+local search_or_create_branch = function (prompt_bufnr, map)
+    actions.select_default:replace(function()
+        actions.close(prompt_bufnr)
+        local selected_entry = action_state.get_selected_entry()
+        local selection = selected_entry[1]
+
+        local git_checkout = string.format("git checkout %s", selection)
+        vim.fn.systemlist(git_checkout)
+    end)
+
+    return true
+end
+
+M.get_branches_or_diff_error = function ()
+    -- check if there is any diff before returning all the branches, otherwise the checkout will fail
+    return vim.fn.systemlist("git branch -a")
+end
+
+M.render_branches_list = function (branches_list)
+    pickers.new({}, {
+        prompt_title = "branches",
+        finder = finders.new_table({
+            results = branches_list
+        }),
+        sorter = conf.generic_sorter({}),
+        attach_mappings = search_or_create_branch,
+    }):find()
+end
+
+return M
--- lua/gitspector/init.lua
+++ lua/gitspector/init.lua
@@ -1,30 +1,9 @@
-local pickers = require "telescope.pickers"
-local finders = require "telescope.finders"
-local conf = require("telescope.config").values
-local actions = require("telescope.actions")
-local action_state = require("telescope.actions.state")
+local branches = require("gitspector.branches")
 
 local M = {}
 
-M.gitspector = function ()
-    local git_branches = vim.fn.systemlist("git branch -a")
-    pickers.new({}, {
-        prompt_title = "branches",
-        finder = finders.new_table({
-            results = git_branches
-        }),
-        sorter = conf.generic_sorter({}),
-        attach_mappings = function(prompt_bufnr, map)
-          actions.select_default:replace(function()
-            actions.close(prompt_bufnr)
-            local selected_entry = action_state.get_selected_entry()
-            local selection = selected_entry[1]
-
-            vim.fn.systemlist(string.format("git checkout %s", selection))
-          end)
-          return true
-        end,
-    }):find()
+M.branches_list = function ()
+    branches.render_branches_list(branches.get_branches_or_diff_error())
 end
 
 return M
