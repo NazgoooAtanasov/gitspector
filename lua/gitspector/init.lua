4952d5f539bf8c1c0e3a790ac0f71b48715f8d76
NazgoooAtanasov <naskodragomirov@gmail.com>
2022-07-30 11:47:47 +0300

    feat: wip telescope select.

--- lua/gitspector/init.lua
+++ lua/gitspector/init.lua
@@ -1,7 +1,27 @@
+local pickers = require "telescope.pickers"
+local finders = require "telescope.finders"
+local conf = require("telescope.config").values
+local actions = require("telescope.actions")
+local action_state = require("telescope.actions.state")
+
 local M = {}
 
 M.gitspector = function ()
-    print("gitspector")
+    pickers.new({}, {
+        prompt_title = "branches",
+        finder = finders.new_table({
+            results = { "red", "green", "blue" }
+        }),
+        sorter = conf.generic_sorter({}),
+        attatch_mappings = function(prompt_bufnr, map)
+            actions.select_default:repalce(function()
+                actions.close(prompt_bufnr)
+                local selection = action_state.get_selected_entry()
+                vim.api.nvim_put({ selection[1] }, "", false, true)
+            end)
+            return true
+        end
+    }):find()
 end
 
 return M
